4ccacd1c9ce5e5c9870f62514b339fc6f0bb6d3f
Amir Dhn <81849499+ksra7900@users.noreply.github.com>
2025-08-25 11:32:48 +0330

    first push

---
+++ autoformer/model.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 25 11:27:39 2025
+
+@author: amir
+"""
+
---
+++ tests/test_layers.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 25 11:28:05 2025
+
+@author: amir
+"""
+
