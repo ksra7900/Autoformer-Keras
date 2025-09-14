297171f8e945bfcb2c9f4f203401e5f0671e94ed
amir <dmohmmadamir@gmail.com>
2025-09-14 20:39:33 +0330

    Finish Autoformer

--- autoformer/layers.py
+++ autoformer/layers.py
@@ -1,6 +1,7 @@
+import math
 import tensorflow as tf
 from keras.models import Model
-from keras.layers import Layer, AveragePooling1D, Input, Dense
+from keras.layers import Layer, AveragePooling1D, Input, Dense, Dropout
 
 # intialize Series Decomposition
 class SeriesDecomposition(Layer):
@@ -33,25 +34,27 @@ class SeriesDecomposition(Layer):
 
 # intialize AutoCorrelation
 class AutoCorrelation(Layer):
-    def __init__(self, d_model, n_heads, c=1, **kwargs):
+    def __init__(self, d_model, n_heads, dropout_rate= 0.1, c=1, **kwargs):
         super(AutoCorrelation, self).__init__(**kwargs)
         self.d_model = d_model
         self.n_heads = n_heads
         self.c = c
+        self.dropout_rate= dropout_rate
         assert d_model % n_heads == 0, "d_model must be divisible by n_heads"
         self.d_head = d_model // n_heads
         
     def build(self, input_shape):
         # Define weight matrices Q,V,K for forecasting
-        self.wq= self.add_weight(shape=(self.d_model, self.d_model),
+        query_shape = input_shape[0] if isinstance(input_shape, list) else input_shape
+        self.wq= self.add_weight(shape=(query_shape[-1], self.d_model),
                                  initializer='glorot_uniform',
                                  trainable= True,
                                  name= 'wq')
-        self.wk= self.add_weight(shape=(self.d_model, self.d_model),
+        self.wk= self.add_weight(shape=(query_shape[-1], self.d_model),
                                  initializer='glorot_uniform',
                                  trainable= True,
                                  name= 'wk')
-        self.wv= self.add_weight(shape=(self.d_model, self.d_model),
+        self.wv= self.add_weight(shape=(query_shape[-1], self.d_model),
                                  initializer='glorot_uniform',
                                  trainable= True,
                                  name= 'wv')
@@ -61,31 +64,44 @@ class AutoCorrelation(Layer):
                                  trainable= True,
                                  name= 'wo')
         
+        self.dropout = Dropout(self.dropout_rate)
         super(AutoCorrelation, self).build(input_shape)
         
-    def roll_function(self, v, tau):
-        batch_size, n_heads, seq_len, d_head= tf.shape(v)[0], tf.shape(v)[1], tf.shape(v)[2], tf.shape(v)[3]
+    def _time_delay_agg(self, values, corr):
+        batch_size = tf.shape(values)[0]
+        n_heads = tf.shape(values)[1]
+        d_head = tf.shape(values)[2]
+        length = tf.shape(values)[3]
         
-        v_flat= tf.reshape(v, (batch_size * n_heads, seq_len, d_head))
-        tau_flat= tf.reshape(tau, (batch_size * n_heads, seq_len))
+        # Find top k delays
+        k= int(self.c * math.log(length))
+        mean_corr= tf.reduce_mean(corr, axis=(1, 2))
+        top_k_vals, top_k_indices= tf.math.top_k(mean_corr, k=k)
         
-        def roll_single_sequence(args):
-            v_vec, tau_vec= args
+        # Apply softmax to get weights
+        weights= tf.nn.softmax(top_k_vals, axis=-1)
+        
+        # time delay aggregation
+        delays_agg= tf.zeros_like(values)
+        for i in range(k):
+            delay_val= top_k_indices[..., i]
             
-            rolled_sequence= []
-            for t in range(seq_len):
-                shift= tau_vec[t]
-                rolled_v= tf.roll(v_vec, shift=shift, axis=0)
-                rolled_sequence.append(rolled_v[t])
+            patterns= []
+            for b in range(batch_size):
+                pattern= tf.roll(values[b], shift=[-delay_val[b]], axis=[-1])
+                patterns.append(pattern)
                 
-            return tf.stack(rolled_sequence, axis=0)
+            pattern = tf.stack(patterns, axis=0)
+            
+            # add suitable weight
+            weight_expanded = tf.reshape(weights[:, i], [batch_size, 1, 1, 1])
+            delays_agg += pattern * tf.cast(weight_expanded, values.dtype)
+            
+        return delays_agg
         
-        output_flat= tf.vectorized_map(roll_single_sequence, (v_flat, tau_flat))
-        output= tf.reshape(output_flat, (batch_size, n_heads, seq_len, d_head))
         
-        return output
-        
-    def call(self, queries, keys, values):
+    def call(self, inputs):
+        queries, keys, values = inputs
         batch_size = tf.shape(queries)[0]
         len_q = tf.shape(queries)[1]
         len_k = tf.shape(keys)[1]
@@ -106,48 +122,68 @@ class AutoCorrelation(Layer):
         V = tf.transpose(V, perm=[0, 2, 3, 1])
         
         # compute FFT 
-        Q_fft = tf.signal.rfft(tf.cast(Q, tf.complex64))
-        K_fft = tf.signal.rfft(tf.cast(K, tf.complex64))
+        Q_fft = tf.signal.rfft(Q)
+        K_fft = tf.signal.rfft(K)
         
         # Compute cross-correlation via inverse FFT of element-wise product
         S = Q_fft * tf.math.conj(K_fft)
         corr = tf.signal.irfft(S)
         corr = tf.math.real(corr)
         
-        # find top K delay
-        k = tf.cast(tf.math.floor(self.c * tf.math.log(tf.cast(len_q, tf.float32))), tf.int32)
+        # delay aggregation
+        agg_output= self._time_delay_agg(V, corr)
         
-        # Get top-k values and indices along the last dimension (time lags)
-        top_k_vals, top_k_indices = tf.math.top_k(corr, k, sorted= True)
-
-        # Apply softmax to top_k_vals to get weights
-        weights = tf.nn.softmax(top_k_vals, axis= -1)
+        # Transpose delay
+        agg_output= tf.transpose(agg_output, perm=[0, 3, 1, 2])
         
-        # Delay Aggregation
-        output= tf.zeros_like(V)
+        # Reshape to (batch_size, seq_len, d_model) 
+        agg_output = tf.reshape(agg_output, (batch_size, len_q, self.d_model))
         
-        for i in range(k):
-            tau= top_k_indices[..., i]
-            w= weights[..., i]
-            
-            rolled_v= self.roll_function(V, tau)
-            
-            w= tf.expand_dims(w, axis=-1)
-            output += w * rolled_v
-            
-        output= tf.transpose(output, perm=[0, 2, 1, 3])
-        output= tf.reshape(output, (batch_size, len_q, self.d_model))
+        # Final linear projection
+        output = tf.matmul(agg_output, self.wo)
+        output = self.dropout(output)
         
-        # create output
-        output= tf.matmul(output, self.wo)
         return output
-
-if __name__ == "__main__":
-    input_layer = Input(shape=(100, 5))  # (seq_len=100, features=5)
-    seasonal, trend = SeriesDecomposition(kernel_size=25)(input_layer)
     
-    x = Dense(64)(seasonal)
-    output_layer = Dense(1)(x)
+    def compute_output_shape(self, input_shape):
+        query_shape = input_shape[0]
+        return (query_shape[0], query_shape[1], self.d_model)
+
+class AutoCorrelationWrapper(Layer):
+    def __init__(self, d_model, n_heads, dropout_rate=0.1, c=1, **kwargs):
+        super(AutoCorrelationWrapper, self).__init__(**kwargs)
+        self.auto_correlation = AutoCorrelation(
+            d_model=d_model, 
+            n_heads=n_heads, 
+            dropout_rate=dropout_rate, 
+            c=c
+        )
+        
+    def call(self, inputs):
+        # inputs should be a list of [queries, keys, values]
+        queries, keys, values= inputs
+        return self.auto_correlation(inputs)
+    
+    def compute_output_shape(self, input_shape):
+        return self.auto_correlation.compute_output_shape(input_shape)
+    
+if __name__ == "__main__":
+    # input
+    input_layer = Input(shape=(100, 5))
+    d_model = 8
+    n_heads = 4
+    
+    # creat Q, V, K
+    projected = Dense(d_model)(input_layer)
+    
+    # AutoCorrelation (self-attention)
+    ac_output = AutoCorrelationWrapper(d_model=d_model, n_heads=n_heads)(
+        [projected, projected, projected]  
+    )
+    
+    seasonal, trend = SeriesDecomposition(kernel_size=25)(ac_output)
+    
+    output_layer = seasonal + trend  
     
     model = Model(inputs=input_layer, outputs=output_layer)
     model.compile(optimizer='adam', loss='mse')
