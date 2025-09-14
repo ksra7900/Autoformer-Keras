de9c9b105d77d9366a2575ff1b72c2735e56c8af
amir <dmohmmadamir@gmail.com>
2025-09-15 00:32:11 +0330

    create blocks

---
+++ autoformer/blocks.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+from keras.models import Model
+from keras.layers import Layer
+from layers import AutoCorrelationWrapper, SeriesDecomposition
+
+class encoder(Layer):
+    def __init__(self):
+        pass
+    
+    
+class decoder(Layer):
+    def __init__(self):
+        pass
+
